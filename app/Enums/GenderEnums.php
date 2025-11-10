2d65bff2e930d28585ceb2a03d1c2c157d0f8b19
KPALIKA <charlesyaovikp@gmail.com>
2025-11-10 10:21:29 +0000

    initial commit

---
+++ app/Enums/GenderEnums.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum GenderEnums: string
+{
+    case MASCULIN = "M";
+    case FEMININ = "F";
+}
---
+++ app/Enums/RoleEnums.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum RoleEnums: string
+{
+    case SYSTEME_ADMIN = "Administrateur du système";
+    case ADMIN = "Administrateur";
+}
